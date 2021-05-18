636f2ed4458155ab2f621031f264c0d29dc1d606
Gevorg Manukyan <gevorg.manukyan@yahoo.com>
2021-05-18 23:13:11 +0400

    added some documentation to routes

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,44 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+| Available methods: get, post, put, patch, delete, options.
+| 
+| Route::match(['get', 'post'], '/', function () {
+| 	//
+| });
+|
+| Route::any('/', function () {
+| 	//
+| });
+|
+| Route::middleware(['first', 'second'])->group(function () {
+|	//
+| });
+|
+|--------------------------------------------------------------------------
+| Subdomains
+|--------------------------------------------------------------------------
+| Route::domain('{account}.example.com')->group(function () {
+|	//
+| });
+|
+|
+|
+|
+|
+|
+|
+
 */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::match(['get', 'post'], '/', function () {
+    //
+});
+
+Route::any('/', function () {
+    //
 });
